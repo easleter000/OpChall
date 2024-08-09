b3a827e4aede05ddfabc7878938992aafb0e9e7b
Terry Easley <easleter000@warren.k12.in.us>
2024-08-09 13:57:34 -0400

    U1L1.4.2

--- src/OpChall.java
+++ src/OpChall.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class OpChall {
+public class OpChall {
+    public static void main(String[] args) {
+        double var = 20;
+        double var2 = 80;
+        double var3 = (var + var2 * 100);
+        System.out.println("myvar3 = " + var3);
+        double theRemainder = var3 % 40.00;
+        System.out.println("theRemainder = " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("isNoRemainder = " + isNoRemainder);
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder");
+
+        }
+    }
 }
